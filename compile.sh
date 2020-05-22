dadcca751dcebb8e1ec951335f9dc4f6c507bd28
rborder <ribo7412@colorado.edu>
2020-05-22 14:09:28 -0600

    cleaned up

--- compile.sh
+++ compile.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-pandoc curc.md -o curc.pdf -V documentclass=extreport -V fontsize=9pt -H header.sty 
-
-cp curc.md README.md
-
-cat footer.txt >> README.md
+pandoc README.md -o ibg_rc_cheatsheet.pdf -V documentclass=extreport -V fontsize=9pt -H header.sty 
